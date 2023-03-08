787216521b4b00d7a25ff76141b84c4a77a3307c
eshapaceka <120716805+eshapaceka@users.noreply.github.com>
2023-03-08 14:20:47 -0500

    Update main.py
    commit - to add comments

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 def encoder(password):
-    new_integer = ''
-    for integer in password:
-        single_int = ((int(integer)) + 3) % 10
-        single_int = str(single_int)
-        new_integer += single_int
-    return new_integer
+    new_integer = ''  # acts as a "list" but a string to develop a new set of integers
+    for integer in password:  # for loop to add three to each digit
+        single_int = ((int(integer)) + 3) % 10  # wraps the integer in int to add three with modulo to isolate the integer
+        single_int = str(single_int). # changes to str to be able to be added
+        new_integer += single_int  # added to the empty string
+    return new_integer  # returns the new integer after the for loop concludes
 
 
 
 def main():
-    while True:
+    while True:  # continues to run the code
         print()
         print('Welcome to Password Coding!')
         print('------------------')
@@ -20,7 +20,7 @@ def main():
         print()
         selection = int(input('Select an option: '))
 
-        if selection in range(1, 3):
+        if selection in range(1, 3):  # ensures numbers other than 1 or 2 are not entered
             password = input('Enter the password: ')
             if selection == 1:
                 print(f'The encoded password is: {encoder(password)}!')
@@ -28,7 +28,7 @@ def main():
             elif selection == 2:
                 print(f'The decoded password is: {decoder(password)}!')
 
-        else:
+        else:  # exits for option 3 and any other number besides 1 and 2
             exit()
 
 
